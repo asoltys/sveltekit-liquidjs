aa20b7f88c4b2b2f2256d87c33ecf8fe1e338568
Adam Soltys <asoltys@gmail.com>
2022-03-10 17:15:11 -0800

    address derivation example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import { fromBase58 } from "bip32";
+import { networks, payments } from "liquidjs-lib";
+let network = networks["liquid"];
+
+let { p2wpkh, p2sh } = payments;
+
+  let pubkey = 
+    "xpub6DYVec9KiT6edxdocFAJmuiKe857uGJC3DTr1c4KMM5w9rQ4CnpH1AieDoSh332jK9dk4jFDD3SmaWhFPdyxoSJAQNYjX1jzJvyAAQf5R54";
+
+  let root = fromBase58(pubkey,
+    network
+  );
+
+  let address;
+  let i = 0;
+  let derive = () => {
+    i++;
+    let pubkey = root.derivePath(i.toString()).publicKey;
+    let redeem = payments.p2wpkh({
+      pubkey,
+      network,
+    });
+
+    ({address} = payments.p2sh({
+      redeem,
+      network,
+    }));
+  }
+
+  derive();
+
+</script>
+
+<button on:click={derive}>Get Address</button>
+{address}
